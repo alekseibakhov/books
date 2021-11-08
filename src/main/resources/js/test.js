d5aceb3f9ed68410b46bfca7838a37d9c547d89a
alekseibakhov <bakhov93@inbox.ru>
2021-11-08 18:04:21 +0300

    подгрузка всех книг из массива удалил ненужные html файлы статичные добавил отдельный файл js для обработки  кликов при обращении к книге

---
+++ src/main/resources/js/test.js
@@ -0,0 +1,28 @@
+const url = new URL(window.location);
+let bookIndex = url.searchParams.get('book_index');
+let books = !localStorage.task ? [] : JSON.parse(localStorage.getItem('task')); // если в браузере уже есть массив, то используем его
+const getBook = document.querySelector('.book_base');
+
+getBook.innerHTML = `
+        <header class="updown">
+                <h1>${books[bookIndex].name}</h1>
+        </header>
+        <div class="main">
+            <div class="leftright"></div>
+            <div class="center">
+                <img class="image" src="img/library.jpg" alt="библиотека" width="200" height="180" align="left">
+                <ul class="list">
+                    <li>${books[bookIndex].name}</li>
+                    <li>${books[bookIndex].author}</li>
+                    <li>${books[bookIndex].date}</li>
+                    <li>${books[bookIndex].genre}</li>
+                    <li>${books[bookIndex].numOfPages}</li>
+                </ul>
+    
+            </div>
+            <div class="leftright"></div>
+        </div>
+        <footer class="updown">
+            <p><a href="book.html"> Назад </a></p>
+        </footer>
+`
